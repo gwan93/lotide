e32513f711673e16b3e272cf33c359304b709563
vagrant <gio_wan@live.ca>
2021-02-01 23:20:37 +0000

    Fixed the emojis

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
 const assertEqual = (actual, expected) => {
   if (actual === expected) {
-    console.log(`🛑🛑🛑Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`✅✅✅Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -16,9 +16,9 @@ const head = (arr) => {
   return arr[0];
 };
 
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(['5',6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Goodbye");
-// assertEqual(head([]), undefined);
-// assertEqual(head(['oneElement']), 'oneElement');
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(['5',6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Goodbye");
+assertEqual(head([]), undefined);
+assertEqual(head(['oneElement']), 'oneElement');
