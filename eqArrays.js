44beecfba20e1c26f6c73749c697883e736ed39a
vagrant <gio_wan@live.ca>
2021-02-02 23:46:36 +0000

    linted

--- eqArrays.js
+++ eqArrays.js
@@ -12,11 +12,10 @@ const eqArrays = (arr1, arr2) => {
       return false;
     }
   }
-  return true
+  return true;
+};
 
-}
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
