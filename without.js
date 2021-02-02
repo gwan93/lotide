e4b06b4950197eaf3aae2d45ba9ab6e22f35f06d
vagrant <gio_wan@live.ca>
2021-02-02 23:49:44 +0000

    a function to filter terms from an array

---
+++ without.js
@@ -0,0 +1,53 @@
+// assertEqual refactored to use eqArrays to compare
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const without = (sourceArr, itemsToRemoveArr) => {
+  let output = [...sourceArr];
+  for (let i = 0; i < itemsToRemoveArr.length; i++) {
+    if (output.includes(itemsToRemoveArr[i])) {
+      output = output.filter(e => e !== itemsToRemoveArr[i]);
+    }
+  }
+  return output;
+};
+
+// Test Cases
+assertArraysEqual(
+  without([1, 2, 3, 3, 3, 4, 'dog', 'dog', '1', '2', '3', '4', 'cat', 1, 2, 5, 6], [3, 'dog', '3', 5]),
+  [1, 2, 4, '1', '2','4', 'cat', 1, 2, 6]
+);
+
+assertArraysEqual(
+  without(['a', 'b', 1, 2], []),
+  ['a', 'b', 1, 2]
+);
+
+assertArraysEqual(
+  without([], ['a']),
+  []
+);
+
+assertArraysEqual(
+  without(['a', 'a', 'a', 'a'], ['a']),
+  []
+);
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
