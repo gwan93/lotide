bbaa15cc60ab8fc12a13b00ebc50fe9f5c91a731
vagrant <gio_wan@live.ca>
2021-02-02 22:50:28 +0000

    Tried to refactor by simply reusing assertEqual and eqArray functions.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -11,6 +11,27 @@ const assertArraysEqual = (actualArr, expectedArr) => {
     return `✅✅✅Assertion Passed: ${actualArr} === ${expectedArr}`
 }
 
+// const assertEqual = (actual, expected) => {
+//   if (actual === expected) {
+//     return `✅✅✅Assertion Passed: ${actual} === ${expected}`;
+//   } else {
+//     return `🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`;
+//   }
+// };
+
+// const assertArraysEqual = (arr1, arr2) => {
+//   if (arr1.length !== arr2.length) {
+//     return assertEqual(arr1.length, arr2.length);
+//   }
+
+//   for (let i = 0; i < arr1.length; i++) {
+//     if (arr1[i] !== arr2[i]) {
+//       return assertEqual(arr1[i], arr2[i]);
+//     }
+//   }
+//   return true;
+// }
+
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
 console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
 console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
