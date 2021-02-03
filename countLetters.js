7525e1446623e64ca329eb06c5525038feb0a6ea
vagrant <gio_wan@live.ca>
2021-02-03 21:46:08 +0000

    a function that counts the occurances of all letters in a string

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (str) => {
+  let output = {};
+  let noSpaces = str.split(' ').join('');
+  
+  for (let char of noSpaces) {
+    if (output[char]) {
+      output[char] += 1;
+    } else {
+      output[char] = 1;
+    }
+  }
+  return output;
+}
+
+console.log(countLetters('LHL')); // L twice, H once
+console.log(countLetters('oowoomoo'));
+
+let result = countLetters('LHL');
+assertEqual(result['L'], 2); // L twice, H once
+assertEqual(result['H'], 1); // L twice, H once
+
+result = countLetters('ass1333!@@@@@')
+assertEqual(result['a'], 1);
+assertEqual(result['s'], 2);
+assertEqual(result['1'], 1);
+assertEqual(result['3'], 3);
+assertEqual(result['!'], 1);
+assertEqual(result['@'], 5);
