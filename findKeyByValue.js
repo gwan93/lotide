1f15868f2804fdfe8233267f7052f07c0f829ea1
vagrant <gio_wan@live.ca>
2021-02-03 22:17:52 +0000

    a function that returns a key based on a value

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = (obj, val) => {
+  // console.log(Object.keys(obj))
+  for (const key of Object.keys(obj)) {
+    // console.log('key', key)
+    if (obj[key] === val) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
