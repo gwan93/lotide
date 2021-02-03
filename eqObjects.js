da48fba06350fd0ce963b70768686306271764de
vagrant <gio_wan@live.ca>
2021-02-03 23:04:27 +0000

    lint

--- eqObjects.js
+++ eqObjects.js
@@ -31,14 +31,13 @@ const eqObjects = (obj1, obj2) => {
     // check if the key's value is an array and compares arrays
     if (Array.isArray(obj1[key]) && Array.isArray(obj2[key])) {
       if (!eqArrays(obj1[key], obj2[key])) {
-        return false
+        return false;
       }
     } else if (obj1[key] !== obj2[key]) {
       return false;
     }
   }
   return true;
-
 };
 
 console.log('first set of tests');
