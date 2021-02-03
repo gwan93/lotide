66b11b46d6d2c5e2ef59ecd5f72a3575c1bf7706
vagrant <gio_wan@live.ca>
2021-02-03 22:01:29 +0000

    a function that returns the indices of each unique letter

---
+++ letterPositions.js
@@ -0,0 +1,33 @@
+const assertArraysEqual = (actual, expected) => {
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (arr1, arr2) => {
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const letterPositions = function(sentence) {
+  const results = {};
+  console.log(sentence);
+
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]] && sentence[i] !== ' ') {
+      results[sentence[i]].push(i);
+    } else if (sentence[i] !== ' ') {
+      results[sentence[i]] = [i];
+    }
+  }
+  return results;
+}
+
+// letterPositions('hello');
+console.log(letterPositions('lighthouse in the house'));
