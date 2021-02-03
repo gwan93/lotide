c2a73712df7bc015bfaed12455c81149dd402572
vagrant <gio_wan@live.ca>
2021-02-03 22:52:15 +0000

    fixed bug in eqArrays to perform a length check

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -8,6 +8,10 @@ const assertArraysEqual = (actual, expected) => {
 };
 
 const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
--- eqArrays.js
+++ eqArrays.js
@@ -7,6 +7,10 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqArrays = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
       return false;
@@ -15,6 +19,7 @@ const eqArrays = (arr1, arr2) => {
   return true;
 };
 
+assertEqual(eqArrays([1, 2], [1, 2, 3]), false); // => false
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
