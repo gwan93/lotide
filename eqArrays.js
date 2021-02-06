d9b548a518a6cf97634abb0e171f10adc055109d
vagrant <gio_wan@live.ca>
2021-02-06 01:17:04 +0000

    updated eqArrays to allow nested arrays

--- eqArrays.js
+++ eqArrays.js
@@ -13,7 +13,13 @@ const eqArrays = (arr1, arr2) => {
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+        if (!eqArrays(arr1[i], arr2[i])) {
+          return false;
+        }
+      } else {
+        return false;
+      }
     }
   }
   return true;
@@ -24,3 +30,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
